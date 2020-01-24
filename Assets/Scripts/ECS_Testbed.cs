155040f24e3a28e5acffbb9040501d936c564705
Marios Koutroumpas <mkoutroumpas@gmail.com>
2020-01-24 21:21:15 +0100

    Mode 2: ECS Entities rendering.

--- Assets/Scripts/ECS_Testbed.cs
+++ Assets/Scripts/ECS_Testbed.cs
@@ -4,14 +4,18 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 public class ECS_Testbed : MonoBehaviour
 {
     private List<ZMoveableObject> _moveableObjects;
+    private EntityManager _entityManager;
 
     [SerializeField]
-    public Transform ObjectToMove;
+    public bool UseJobSystem = true;
+    [SerializeField]
+    public GameObject ObjectToMove;
     [SerializeField]
     public int NumberOfObjects = 1000;
     [SerializeField]
@@ -25,17 +29,24 @@ public class ECS_Testbed : MonoBehaviour
     {
         _moveableObjects = new List<ZMoveableObject>();
 
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var entity = GameObjectConversionUtility.ConvertGameObjectHierarchy(ObjectToMove, settings);
+
         for (var i = 0; i < NumberOfObjects; i++)
         {
-            Transform instance = Instantiate(
-                ObjectToMove,
-                new Vector3(UnityEngine.Random.Range(-XSpread, XSpread),
-                            UnityEngine.Random.Range(-YSpread, YSpread),
-                            UnityEngine.Random.Range(-ZSpread, ZSpread)), Quaternion.identity);
+            var position = new Vector3(
+                UnityEngine.Random.Range(-XSpread, XSpread),
+                UnityEngine.Random.Range(-YSpread, YSpread),
+                UnityEngine.Random.Range(-ZSpread, ZSpread));
+
+            var entityInstance = _entityManager.Instantiate(entity);
+            _entityManager.SetComponentData(entityInstance, new Translation { Value = position });
 
             _moveableObjects.Add(new ZMoveableObject
             {
-                Transform = instance,
+                Entity = entityInstance,
+                Position = position,
                 Velocity = UnityEngine.Random.Range(0.1f, 1.0f)
             });
         }
@@ -45,10 +56,45 @@ public class ECS_Testbed : MonoBehaviour
     {
         float startTime = Time.realtimeSinceStartup;
 
-        for (var i = 0; i < _moveableObjects.Count; i++)
+        if (UseJobSystem)
         {
-            _moveableObjects[i].Transform.position += new Vector3(0f, 0f, -_moveableObjects[i].Velocity * Time.deltaTime);
-            Helpers.AddDummyHeavyTask();
+            var _velocities = new NativeArray<float>(_moveableObjects.Count, Allocator.TempJob);
+            var _positions = new NativeArray<float3>(_moveableObjects.Count, Allocator.TempJob);
+
+            for (var i = 0; i < _moveableObjects.Count; i++)
+            {
+                _positions[i] = _moveableObjects[i].Position;
+                _velocities[i] = _moveableObjects[i].Velocity;
+            }
+
+            var job = new ZTranslationJob
+            {
+                MoveSpeeds = _velocities,
+                Positions = _positions,
+                DeltaTime = Time.deltaTime,
+                MinusDirection = true
+            };
+
+            var jobHandle = job.Schedule(_moveableObjects.Count, 10);
+            jobHandle.Complete();
+
+            for (var i = 0; i < _moveableObjects.Count; i++)
+            {
+                _moveableObjects[i].Position = _positions[i];
+
+                _entityManager.SetComponentData(_moveableObjects[i].Entity, new Translation { Value = _moveableObjects[i].Position });
+            }
+
+            _velocities.Dispose();
+            _positions.Dispose();
+        }
+        else
+        {
+            for (var i = 0; i < _moveableObjects.Count; i++)
+            {
+                _moveableObjects[i].Position += new Vector3(0f, 0f, -_moveableObjects[i].Velocity * Time.deltaTime);
+                Helpers.AddDummyHeavyTask();
+            }
         }
 
         Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
@@ -56,7 +102,8 @@ public class ECS_Testbed : MonoBehaviour
 
     public class ZMoveableObject
     {
-        public Transform Transform;
+        public Entity Entity;
+        public Vector3 Position;
         public float Velocity;
     }
 }
